7aa08a26f9e83bae3cd1d9c6a72124e45e1c5460
Averyanov Ivan <xorrou@inbox.ru>
2020-10-01 06:26:43 +0500

    Добавил в поэкт наследуемые классы List и Items

--- Students/Ivan Averyanov/Project/src/components/basket.js
+++ Students/Ivan Averyanov/Project/src/components/basket.js
@@ -1,61 +1,13 @@
-class BasketIint{
-    constructor(item){
-        this.item= item;
-    }
-    render(init){
-        return   `            
-        <div class="cart-list__product">
-                <a href="product.html" class="cart-list__product_image"><img src="${this.item.productImg}" alt="tee-shirt"></a>
-                    <div class="cart-list__product_data">
-                        <a href="product.html"><h3>${this.item.productName}</h3></a>
-                            <div class="star-rating">
-                                <img src="../src/assets/img/promo/star-rating.png" alt="star-rating">    
-                            </div>
-                            <span><span class="product-count" data-id="${this.item.productIdd}">${this.item.amount} </span>x $ ${this.item.productPrice}</span>
-            </div>
-            <input class="product-remove-btn" 
-                data-id="${this.item.productId}" 
-                type="image" 
-                src="../src/assets/img/promo/arrow_cancel.png"
-                name="remove" 
-                alt="arrow_cancel">
-        </div>       
-        `
-    }
-}
+import List from './LIST.js';
 
-export default class Basket {
+export default class Basket extends List {
     constructor(url = '/basket.json', container='.cart-list-content',sumTotal= '.cart-list__total-span' ){
-        this.items= [];
-        this.container = document.querySelector(container);
-        this.shown= false;
-        this.url= 'https://raw.githubusercontent.com/xorrou1/responses/master/JSON%20carts%20marcet' + url;
-        this.sumTotal = document.querySelector(sumTotal);
-        this.init();
+        super(url,container)
+            this.shown= false;
+            this.sumTotal = document.querySelector(sumTotal);
+
     }
     
-    init() {       
-        this._get(this.url)
-            .then(basket => {
-                this.items = basket.content;
-            })
-            .finally(() => {
-                this._render();
-                this._handleActions();
-            })
-    }
-    _get(url) {
-        return fetch(url).then(d => d.json());
-    }
-    _render() {
-        let htmlStr = '';
-        this.items.forEach(item => {
-            htmlStr += new BasketIint(item).render() 
-        });
-
-        this.container.innerHTML = htmlStr;
-        this.renderTotalSum();
-    }
     _handleActions() {
         this.container.addEventListener('click', ev => {
             if (ev.target.name == 'remove') {
@@ -96,4 +48,3 @@ export default class Basket {
 
 }
 
-/* let basket = new Basket(); */
